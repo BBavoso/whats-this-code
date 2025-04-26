7b3f7cde7c28f4d889754a655483e1113670c109
MarkoJ0621 <markolemesos@gmail.com>
2025-04-26 14:31:24 -0400

    lkn

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { GoogleGenAI } from 'https://cdn.jsdelivr.net/npm/@google/genai@0.10.0/dist/node/index.min.js';
+import { GoogleGenAI } from '@google/genai';
 const GEMINI_API_KEY = process.env.super_secret_key;
 
 
-const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
+const ai = new GoogleGenAI({ apiKey: "AIzaSyDwYwlAV2A3eyVi1x68oy3zPB2mx1U641A" });
 
 
 async function translate() {
+    const input = document.querySelector("#input_area");
+    const language = document.querySelector("#language_selector"); //doesn't exist yet shhhh
     const response = await ai.models.generateContent({
         model: 'gemini-2.0-flash-001',
         contents: 'Hey gemini! I was wondering if you can translate this code into ' + language + 'for me: ' + input + 'I just want the code, nothing else',
@@ -19,7 +21,7 @@ async function explain() {
         model: 'gemini-2.0-flash-001',
         contents: 'can you explain this code to me in the simplest way possible?' + input,
     });
-    console.log(response.text);
+    window.alert("HI");
 }
 
 
