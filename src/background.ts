1db7f563e21d08a2c73881581f8134c80572036d
Alexander Hamilton <alexander.2.hamilton@uconn.edu>
2025-04-26 15:47:25 -0400

    added stack overflow search

--- src/background.ts
+++ src/background.ts
@@ -1,17 +1,40 @@
+const CONTEXT_MENUS = [
+  {
+    id: 'searchGithub',
+    title: 'Search GitHub for "%s"',
+    url: (query: string) => `https://github.com/search?q=${query}&type=code`,
+  },
+  {
+    id: 'searchStackOverflow',
+    title: 'Search StackOverflow for "%s"',
+    url: (query: string ) => `https://stackoverflow.com/search?q=${query}`,
+  },
+];
+
+
 chrome.runtime.onInstalled.addListener(() => {
   console.log('Extension installed!');
 
-  chrome.contextMenus.create({
-    id: 'searchGithub',
-    title: 'Search GitHub for "%s"', // %s is replaced with highlighted text
-    contexts: ['selection'], // Only show when user selects text
+  CONTEXT_MENUS.forEach((menu) => {
+    chrome.contextMenus.create({
+      id: menu.id,
+      title: menu.title,
+      contexts: ["selection"]
+    })
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'searchGithub' && info.selectionText) {
-    const query = encodeURIComponent(info.selectionText); // URL encode the selection text
-    const githubSearchUrl = `https://github.com/search?q=${query}&type=code`; // GitHub search with code filter
-    chrome.tabs.create({ url: githubSearchUrl }); // Open GitHub search in a new tab
+
+  const clickedMenu = CONTEXT_MENUS.find((menu) => menu.id === info.menuItemId);
+  if (clickedMenu && info.selectionText) {
+    const query = encodeURIComponent(info.selectionText);
+    const url = clickedMenu.url(query);
+
+    chrome.tabs.create({ url }, (tab) => {
+      if (chrome.runtime.lastError) {
+        console.error('Failed to open tab:', chrome.runtime.lastError.message);
+      }
+    });
   }
 });
